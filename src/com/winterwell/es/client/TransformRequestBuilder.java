d5006f51d90a17dc6542390b60e6e04757620d76
Daniel Winterstein <daniel@good-loop.com>
2020-11-18 10:20:34 +0000

    a few notes + merged in latest from master

--- src/com/winterwell/es/client/TransformRequestBuilder.java
+++ src/com/winterwell/es/client/TransformRequestBuilder.java
@@ -1,23 +1,26 @@
 package com.winterwell.es.client;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.winterwell.utils.Utils;
 import com.winterwell.utils.containers.ArrayMap;
 
 /**
  * Transform indices to merge fields.
  * See https://www.elastic.co/guide/en/elasticsearch/reference/7.9/transforms.html
+ * @testedby TransformRequestBuilderTest
  */
-
 public class TransformRequestBuilder extends ESHttpRequest<TransformRequestBuilder, IESResponse> {
 	
-	// called when a transform preview is requested
+	/** called when a transform preview is requested */
 	public TransformRequestBuilder(ESHttpClient esHttpClient) {
 		super(esHttpClient, "_transform/_preview");
 		setIndex(null); 
 		method = "POST";
 	}
 	
-	// called when creating and starting a transform job request
+	/** called when creating and starting a transform job request */
 	public TransformRequestBuilder(ESHttpClient esHttpClient, String transform_job, String request) {
 		super(esHttpClient, "_transform/"+transform_job);
 		setIndex(null); 
@@ -25,27 +28,37 @@ public class TransformRequestBuilder extends ESHttpRequest<TransformRequestBuild
 	}
 	
 	/**
-	 * Set the body of a transform request
-	 * @param src index source
-	 * @param dest index destination
-	 * @param terms a list of terms which you want to group by
-	 * @param interval if want to group by date histogram, specify interval, else pass empty string
+	 * Set the body of a transform request. Uses count aggregations (what about sum??)
+	 * @param srcIndex index source (where the data comes from)
+	 * @param destIndex index destination (where the aggregated data will go)
+	 * @param terms the terms which you want to group by (i.e. what fields will get kept)
+	 * @param interval (optional) if want to group by date histogram, specify interval format?? e.g. "1d"??
+	 * This is locked to the `time` field ??do we want more flexibility
 	 * @return this
 	*/
-	public TransformRequestBuilder setBody(String src, String dest, ArrayList<String> terms, String interval) {
-		ArrayMap group_by = new ArrayMap();
+	public TransformRequestBuilder setBody(String srcIndex, String destIndex, List<String> terms, String interval) {		
+		// counts ??do we want sums in places??
 		ArrayMap aggregations = new ArrayMap("count", new ArrayMap("value_count", new ArrayMap("field", "count")));
+
+		ArrayMap group_by = new ArrayMap();
 		for (String term : terms) {
 			group_by.put(term, new ArrayMap("terms", new ArrayMap("field", term)));
 		}
-		if (interval != "") {
-			group_by.put("time", new ArrayMap("date_histogram", new ArrayMap("field", "time", "fixed_interval", interval)));
+		if ( ! Utils.isBlank(interval)) {
+			group_by.put("time", new ArrayMap("date_histogram", new ArrayMap(
+					"field", "time", 
+					"fixed_interval", interval
+					)));
 		}
 		//Map 
 		setBodyMap(new ArrayMap(
-			"source", new ArrayMap("index", src),
-			"dest", new ArrayMap("index", dest),
-			"pivot", new ArrayMap("group_by", group_by, "aggregations", aggregations)));
+			"source", new ArrayMap("index", srcIndex),
+			"dest", new ArrayMap("index", destIndex),
+			"pivot", new ArrayMap(
+					"group_by", group_by, 
+					"aggregations", aggregations
+					)
+			));
 		return this;
 	}
 
