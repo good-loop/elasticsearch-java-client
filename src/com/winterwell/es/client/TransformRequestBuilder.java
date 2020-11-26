0e8248148aa4937c07191e7235a37f4150ba46fc
kai <kaijuneer@gmail.com>
2020-11-26 10:36:54 +0000

    create set body function which supports painless script

--- src/com/winterwell/es/client/TransformRequestBuilder.java
+++ src/com/winterwell/es/client/TransformRequestBuilder.java
@@ -48,11 +48,49 @@ public class TransformRequestBuilder extends ESHttpRequest<TransformRequestBuild
 	*/
 	public TransformRequestBuilder setBody(String srcIndex, String destIndex, List<String> terms, String interval) {		
 		// counts ??do we want sums in places??
-		ArrayMap aggregations = new ArrayMap("count", new ArrayMap("value_count", new ArrayMap("field", "count")));
+		ArrayMap aggregations = new ArrayMap("count", new ArrayMap("sum", new ArrayMap("field", "count")));
 
 		ArrayMap group_by = new ArrayMap();
 		for (String term : terms) {
 			group_by.put(term, new ArrayMap("terms", new ArrayMap("field", term)));
+		}
+		if ( ! Utils.isBlank(interval)) {
+			group_by.put("time", new ArrayMap("date_histogram", new ArrayMap(
+					"field", "time", 
+					"fixed_interval", interval
+					)));
+		}
+		//Map 
+		setBodyMap(new ArrayMap(
+			"source", new ArrayMap("index", srcIndex),
+			"dest", new ArrayMap("index", destIndex),
+			"pivot", new ArrayMap(
+					"group_by", group_by, 
+					"aggregations", aggregations
+					)
+			));
+		return this;
+	}
+	
+	/**
+	 * Set the body of a transform request. Does not ignore documents with null fields
+	 * @param srcIndex index source (where the data comes from)
+	 * @param destIndex index destination (where the aggregated data will go)
+	 * @param terms the terms which you want to group by (i.e. what fields will get kept)
+	 * @param interval (optional) if want to group by date histogram, specify interval format, e.g. "24h"
+	 * @return this
+	*/
+	public TransformRequestBuilder setBodyWithPainless(String srcIndex, String destIndex, List<String> terms, String interval) {		
+		// counts ??do we want sums in places??
+		ArrayMap aggregations = new ArrayMap("count", new ArrayMap("sum", new ArrayMap("field", "count")));
+
+		ArrayMap group_by = new ArrayMap();
+		String script;
+		for (String term : terms) {
+			script = "if (doc[\u0027"+term+"\u0027].size() == 0) {return \"\";}return doc[\u0027"+term+"\u0027].value;";
+			group_by.put(term, new ArrayMap("terms", new ArrayMap("script", new ArrayMap(
+					"source", script,
+					"lang", "painless"))));
 		}
 		if ( ! Utils.isBlank(interval)) {
 			group_by.put("time", new ArrayMap("date_histogram", new ArrayMap(
