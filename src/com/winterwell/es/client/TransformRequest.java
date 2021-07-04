ba91f06ffb25e9b5ae1a55e1addefb7152f9e8d6
Daniel Winterstein <daniel@good-loop.com>
2021-07-04 09:42:32 +0100

    comment

--- src/com/winterwell/es/client/TransformRequest.java
+++ src/com/winterwell/es/client/TransformRequest.java
@@ -74,7 +74,7 @@ public class TransformRequest extends ESHttpRequest<TransformRequest, IESRespons
 		return this;
 	}
 	
-	// Similar to setBody function, but with painless script support in case for ES version < 7.10.0
+	/** Similar to setBody function, but with painless script support in case for ES version < 7.10.0 */
 	public TransformRequest setBodyWithPainless(String srcIndex, String destIndex, List<String> aggs, List<String> terms, String interval) {		
 		ArrayMap aggregations = new ArrayMap("count", new ArrayMap("sum", new ArrayMap("field", "count")));
 		for (String agg : aggs) {
